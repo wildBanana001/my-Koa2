0aca65a042b38b37956bb268c99b0c043b8a3a1e
林凯锋 <linkaifeng.kfc@bytedance.com>
2019-07-30 00:56:50 +0800

    koa2

--- application.js
+++ application.js
@@ -1,15 +1,57 @@
 const http = require('http')
 
+let request = {
+	get url() {
+		return this.req.url
+	}
+}
+
+let response = {
+	get body() {
+		return this._body
+	},
+	set body(val) {
+		this._body = val
+	}
+}
+
+let context = {
+	get url() {
+		return this.request.url
+	},
+	get body() {
+		return this.response.body
+	},
+	set body(val) {
+		this.response.body = val
+	}
+}
+
 class Application {
 	constructor() {
-		this.callback = () => {}
+		this.context = context
+		this.request = request
+		this.response = response
 	}
 	use(callback) {
 		this.callback = callback
 	}
+	createCtx(req, res) {
+		let ctx = Object.create(this.context)
+		ctx.request = Object.create(this.request)
+		ctx.response = Object.create(this.response)
+		ctx.req = ctx.request.req = req
+		ctx.res = ctx.response.res = res
+		return ctx
+	}
+	async transformCtx(req, res) {
+		let ctx = this.createCtx(req, res)
+		await this.callback(ctx)
+		ctx.res.end(ctx.body)
+	}
 	listen(...args) {
 		const server = http.createServer((req, res) => {
-			this.callback(req, res)
+			this.transformCtx(req, res)
 		})
 		server.listen(...args)
 	}
--- server.js
+++ server.js
@@ -1,22 +1,9 @@
 const myKoa = require('./application')
 const app = new myKoa()
 
-app.use((req, res) => {
-	res.writeHead(200)
-	res.end('hello Koa2')
+app.use(async ctx => {
+	ctx.body = `<h1>hello Koa2 ${ctx.url}</h1>`
 })
 app.listen(3002, () => {
 	console.log('server start on port 3002')
 })
-
-
-// const http = require('http')
-
-// const server = http.createServer((req, res) => {
-// 	res.writeHead(200)		//响应状态码 
-// 	res.end('hello Koa2')
-// })
-
-// server.listen(9999, () => {
-// 	console.log('server start on port 9999')
-// })
